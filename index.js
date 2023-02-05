7e120759aa84dcd8c1558139818dfaff4fb28407
nathalia-uf <112099626+nathalia-uf@users.noreply.github.com>
2023-02-05 18:33:00 -0500

    trial

--- index.js
+++ index.js
@@ -1,39 +1,25 @@
-function runQuiz(){
-    console.log('start-quiz')
-    var questionsArr = [
-        {
-          question: 'The cup half full',
-          answer: true
-        }
-        {
-          question: 'Today a good day',
-          answer: true
-        }
-        {
-        question: 'You enjoy your job',
+function runQuiz() {
+var questionsArr = [
+    {
+        question: 'Your cup is half full'
         answer: true
-        }
-        {
-        question: 'You are currently smiling',
-        answer: true
-        }
-        {
-        question: 'You prefer JavaScript',
-        answer: true
-        }
-    ]
-    var numOfAgrees = 0
-    for (var i = 0; i < questionsArr.length; i++) {
-        var question = questionsArr [i]
-        console.log(question)
-        var answer = confirm(question)
-        if (answer) {
-            numOfAgrees++
-        }
     }
-    if(numOfAgrees >= 3) {
-        alert("2023 is going to be your year!")
-    } else {
-        alert("2023 is not your year :(")
+    {
+        question: 'You are smiling'
+        answer:true
     }
+    {
+        question: 'You enjoy this class'
+        answer: true
+    }
+    {
+        question: 'You like your current job'
+        answer: true
+    }
+    {
+        question: 'JavaScript is awesome'
+        answer: true
+    }]
 }
+
+
