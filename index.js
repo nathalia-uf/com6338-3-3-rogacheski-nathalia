7bd4c33bc0bd6328bd9c416ffd70f42dba8c50f3
nathalia-uf <112099626+nathalia-uf@users.noreply.github.com>
2023-02-05 11:00:18 -0500

    questions

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function runQuiz() {
+    var questionsArr = [
+        {
+          question: 'Is the cup half full?',
+          answer: true
+        }
+    ]
+    var questionsArr = [
+        {
+          question: 'Is today a good day?',
+          answer: true
+        }
+    ]  
+    var questionsArr = [
+    {
+      question: 'Do you like your job?',
+      answer: true
+    }
+    ]
+    var questionsArr = [
+    {
+      question: 'Are you smiling?',
+      answer: true
+    }
+    ]
+    var questionsArr = [
+    {
+      question: 'Do you like Javascript?',
+      answer: true
+    }
+    ]
+    for (var i = 0; i < questionsArr.length; i++) {
+        var question = questionsArr
+        var answer = confirm
+
+    }
+}
