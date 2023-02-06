344ebdad6fdbfa0532fcecbf877e1530051dd8b5
nathalia-uf <112099626+nathalia-uf@users.noreply.github.com>
2023-02-05 21:36:07 -0500

    questions setup

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 function runQuiz() {
-var questionsArr = [
-    {
-        question: 'Your cup is half full'
-        answer: true
-    }
-    {
-        question: 'You are smiling'
-        answer:true
-    }
-    {
-        question: 'You enjoy this class'
-        answer: true
-    }
-    {
-        question: 'You like your current job'
-        answer: true
-    }
-    {
-        question: 'JavaScript is awesome'
-        answer: true
-    }]
-}
+    var questionsArr = [{
+            question: "Your cup is half full", 
+            answer: true
+        },
+        {   question: 'You are smiling', 
+            answer: true
+        },
+        {   question: 'You enjoy this class', 
+            answer: true
+        },
+        {   question: 'You like your current job', 
+            answer: true
+        },
+        {   question: 'JavaScript is awesome', 
+            answer: true
+        }
+    ]
 
+    var numOfAgrees = 0
+    for(var i = 0; i < questionsArr.length; i++) {
+        var question = questionsArr[i]
+        var answer = confirm(question)
+        if (answer) {
+            numOfAgrees++
+        }
+    }
+
+    if (numOfAgrees >= 3) {
+        alert("2023, is your year :)")
+    } else {
+        alert("2023,is not your year :(")
+    }
+}
 
